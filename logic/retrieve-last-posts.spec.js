be418cdc1b7b955d634e9fa0d0d906e44da5bd1f
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-04-19 12:00:29 +0200

    Logic and spec for last posts started - added general styles file

---
+++ logic/retrieve-last-posts.spec.js
@@ -0,0 +1,47 @@
+const config = require('../config')
+const { retrieveLastPosts } = require('.')
+const fetch = require('node-fetch')
+const { API_URL } = require('../config')
+
+const logic = require('.')
+
+logic.__context__.API_URL = config.API_URL
+
+describe.only('retrieve-last-posts', () => {
+
+    let orderedDates
+
+    beforeEach(async () => {
+        const result = await fetch(API_URL)
+
+        const res = await result.json()
+
+        orderedDates = res.data.children.map(el => el.data.created_utc).sort((a, b) => a - b)
+    })
+
+    it('should succeed on showing last posts', async () => {
+        const posts = await retrieveLastPosts()
+
+        // console.log(posts)
+
+        const postsDates = posts.map(el => el.created_utc)
+
+        for (let i = 0; i < posts.length; i++) {
+
+            expect(posts[i].created_utc).toBe(orderedDates[i])
+            expect(posts[i].title).toBeDefined()
+            expect(posts[i].thumbnail).toBeDefined()
+            expect(posts[i].author).toBeDefined()
+            expect(posts[i].id).toBeDefined()
+            expect(posts[i].score).toBeDefined()
+            expect(posts[i].num_comments).toBeDefined()
+            expect(posts[i].permalink).toBeDefined()
+        }
+
+    })
+
+    // it('should fail on network error', () => {
+
+    // })
+
+})
