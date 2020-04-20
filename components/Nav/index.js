a0182a85e061b6f7a0caaa376a8182859294f008
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-04-20 21:31:38 +0200

    Highlighted navbar

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,22 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from './style'
 import { TouchableOpacity, Text, View } from "react-native"
 
-export default Nav = ({ goToLastPosts, goToTopPosts, goToHotPosts, goToPolemicalPosts }) => {
+export default Nav = ({ goToLastPosts, goToTopPosts, goToOldPosts, goToHotPosts }) => {
+
+    const [pressed, setPressed] = useState('last')
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.option} onPress={() => goToLastPosts()}>
+            <TouchableOpacity style={pressed === 'last' ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToLastPosts(); return setPressed('last') }}>
                 <Text style={styles.option_text}>LAST</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.option} onPress={() => goToTopPosts()}>
+            <TouchableOpacity style={pressed === 'top' ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToTopPosts(); return setPressed('top') }}>
                 <Text style={styles.option_text}>TOP</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.option} onPress={() => goToHotPosts()}>
+            <TouchableOpacity style={pressed === 'hot' ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToHotPosts(); return setPressed('hot') }}>
                 <Text style={styles.option_text}>HOT</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.option} onPress={() => goToPolemicalPosts()}>
-                <Text style={styles.option_text}>POLEMICAL</Text>
+            <TouchableOpacity style={pressed === 'older' ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToOldPosts(); return setPressed('older') }}>
+                <Text style={styles.option_text}>OLDER</Text>
             </TouchableOpacity>
         </View>
     )
