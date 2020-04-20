cc13cbdaed4935b2245ddab62723627e6643619f
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-04-20 13:37:57 +0200

    Changed logic for posts to App

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Posts, Detail } from './components'
 import { StatusBar, View, ImageBackground, ActivityIndicator, Text } from 'react-native'
 import logic from './logic'
@@ -12,6 +12,20 @@ export default App = () => {
   const [error, setError] = useState(undefined)
   const [view, setView] = useState('landing')
   const [postLink, setPostLink] = useState()
+  const [posts, setPosts] = useState()
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const posts = await retrieveLastPosts()
+        setPosts(posts)
+      }
+      catch (error) {
+        console.log(error)
+        setError(error)
+      }
+    })()
+  }, [])
 
   const handleGoToLink = (link) => {
     !postLink ? setPostLink(link) : setPostLink(undefined)
