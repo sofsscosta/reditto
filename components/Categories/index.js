eb3a7d711f2e3f31b8514ec9fdc5cf849c19d9ff
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-04-25 11:43:42 +0200

    Refactoring Categories

--- components/Categories/index.js
+++ components/Categories/index.js
@@ -1,26 +1,29 @@
 import React, { useState } from "react"
 import styles from './style'
-import { TouchableOpacity, Text, View } from "react-native"
+import { TouchableOpacity, FlatList, Text, View } from "react-native"
 import { last, top, old, polemical } from '../../logic/category'
 
-export default Nav = ({ goToPosts }) => {
+export default Categories = ({ goToPosts }) => {
 
     const [pressed, setPressed] = useState('last')
+    const categories = [
+        { name: 'last', category: last, id: '0' },
+        { name: 'top', category: top, id: '1' },
+        { name: 'polemical', category: polemical, id: '2' },
+        { name: 'older', category: old, id: '3' },
+    ]
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={pressed === 'last' ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToPosts(last); setPressed('last') }}>
-                <Text style={styles.option_text}>LAST</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={pressed === 'top' ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToPosts(top); setPressed('top') }}>
-                <Text style={styles.option_text}>TOP</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={pressed === 'polemical' ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToPosts(polemical); setPressed('polemical') }}>
-                <Text style={styles.option_text}>POLEMICAL</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={pressed === 'older' ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToPosts(old); setPressed('older') }}>
-                <Text style={styles.option_text}>OLDER</Text>
-            </TouchableOpacity>
+            <FlatList
+                horizontal={true}
+                data={categories}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <TouchableOpacity style={pressed === item.name ? styles.option : [styles.option, styles.option_pressed]} onPress={() => { goToPosts(item.category); setPressed(item.name) }}>
+                        <Text style={styles.option_text}>{item.name.toUpperCase()}</Text>
+                    </TouchableOpacity>
+                )} />
         </View>
     )
 }
